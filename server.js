d94e739c73802fdb61d5d72f854bddc3231e5c46
Liliia Khrystiuk <liliya.khrystyuk@gmail.com>
2021-06-23 16:18:31 +0200

    mini lab Dynamic Templating

--- server.js
+++ server.js
@@ -12,12 +12,32 @@ app.use(express.static('public'));
 
 // 1: in the home,list all the students who took the exam (list all the students)
 
-// ... Your code here
+let {students} = require('./exam-info');
+
+app.get("/", (req, res) => {
+  res.render('full-list.hbs', {students})
+});
 
 // 2: in the '/results' list all the students who passed the test and their score.
 // Also, students should be in descending order based on their score.
 
-// ... Your code here
+app.get("/results", (req, res) => {
+  let passed = students.filter((student) => {
+    return student.hasPassed === true
+  });
+  let cloned = JSON.parse(JSON.stringify(passed))
+  let sorted = cloned.sort((a,b) => {
+    if (a.score > b.score) {
+      return -1
+    }else if(a.score < b.score){
+      return 1
+    }else{
+      return 0
+    }
+  })
+  console.log(sorted)
+  res.render('results.hbs', {sorted})
+});
 
 app.listen(process.env.PORT, () =>
   console.log(`App running on ${process.env.PORT}.`)
